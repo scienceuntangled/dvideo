01aeaeae776bd32f60e6c34d4df38c820a3dc493
Ben Raymond <ben@theraymonds.org>
2020-02-13 18:48:19 +1100

    allow extra controls to be passed to dv_video_player

--- R/player.R
+++ R/player.R
@@ -19,7 +19,7 @@ dv_video_js <- function(youtube = FALSE) {
 #'
 #' @param id string: the id of the tag
 #' @param type string: either "youtube" or "local"
-#' @param controls logical: add "previous", "next", and "stop" buttons
+#' @param controls logical: add "previous", "next", "pause", and "stop" buttons. If `controls` is an object of class `shiny.tag` (created by `htmltools::tags`) or `shiny.tag.list` (`htmltools::tagList`) then it will be appended to the controls
 #' @param ... : other attributes of the player element (passed to the player `tags$div` call for youtube or `tags$video` for local)
 #'
 #' @return HTML tags. The outermost element is a div with id `paste0(id, "_container")`, with the player and optionally buttons nested within it.
@@ -42,12 +42,30 @@ dv_video_js <- function(youtube = FALSE) {
 #'       ),
 #'       server = function(input, output) {},
 #'   )
+#'
+#'   shinyApp(
+#'       ui = fluidPage(
+#'           dv_video_js(youtube = TRUE),
+#'           dv_video_player(id = "yt_player", type = "youtube",
+#'                           style = "height: 480px; background-color: black;",
+#'                           controls = tags$button("Go",
+#'                                        onclick = dv_playlist_as_onclick(playlist, "yt_player")))
+#'       )),
+#'       server = function(input, output) {},
+#'   )
 #' }
 #'
 #' @export
 dv_video_player <- function(id, type, controls = FALSE, ...) {
     assert_that(is.string(type))
     type <- match.arg(tolower(type), c("youtube", "local"))
+    if (inherits(controls, c("shiny.tag", "shiny.tag.list"))) {
+        extra_controls <- controls
+        controls <- TRUE
+    } else {
+        if (!is.flag(controls)) stop("controls should be either TRUE/FALSE or a tags/tagList object")
+        extra_controls <- NULL
+    }
     assert_that(is.flag(controls), !is.na(controls))
     if (type == "youtube") {
         plyr <- do.call(tags$div, c(list(id = id), list(...)))
@@ -56,7 +74,7 @@ dv_video_player <- function(id, type, controls = FALSE, ...) {
         ##if (controls) plyr <- htmltools::tagAppendAttributes(plyr, controls = "controls")
     }
     if (controls) {
-        tags$div(id = paste0(id, "_container"), plyr, tags$div(tags$button("Prev", onclick = "dvjs_video_prev();"), tags$button("Next", onclick = "dvjs_video_next();"), tags$button("Pause", onclick = "dvjs_video_pause();"), tags$button("Stop", onclick = "dvjs_video_stop();")))
+        tags$div(id = paste0(id, "_container"), plyr, tags$div(tags$button("Prev", onclick = "dvjs_video_prev();"), tags$button("Next", onclick = "dvjs_video_next();"), tags$button("Pause", onclick = "dvjs_video_pause();"), tags$button("Stop", onclick = "dvjs_video_stop();"), extra_controls))
     } else {
         tags$div(id = paste0(id, "_container"), plyr)
     }
